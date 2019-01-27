a81f093196e321c2b7630bd90dd3e2ea715a4b3b
root <wgoulet@gmail.com>
2019-01-27 09:02:52 +0000

    Added support for updating default doc with other vhosts

--- setupnewhost.py
+++ setupnewhost.py
@@ -5,6 +5,7 @@ import shutil
 import fileinput
 import os
 import datetime
+import bs4
 
 def main(argv):
     dnsname = ''
@@ -19,14 +20,17 @@ def main(argv):
         if opt in ("-d","--dnsname"):
             dnsname = arg
 
+    defaultdoc = '/var/www/html/index.nginx-debian.html'
     templatefile = '/etc/nginx/sites-available/template'
     sitefile = '/etc/nginx/sites-available/{}'.format(dnsname)
     defaulttemplatefile = '/etc/nginx/sites-available/deftemplate'
+    defaultfile = '/etc/nginx/sites-available/default'
+    shutil.copyfile(defaulttemplatefile,defaultfile)
     shutil.copyfile(templatefile,sitefile)
     templatewwwfile = '/var/www/html/template.html'
     sitewwwfile = '{}.html'.format(dnsname)
     shutil.copyfile(templatewwwfile,'/var/www/html/{}'.format(sitewwwfile))
-    with fileinput.input(files=(sitefile,'/var/www/html/{}'.format(sitewwwfile),defaulttemplatefile),inplace=True) as f:
+    with fileinput.input(files=(sitefile,'/var/www/html/{}'.format(sitewwwfile),defaultfile),inplace=True) as f:
         for line in f:
             if(line.count("REPLACEMENAME") > 0):
                 newline = line.replace("REPLACEMENAME",dnsname)
@@ -37,7 +41,15 @@ def main(argv):
             else:
                 print(line,end='')
 
-    shutil.copyfile(defaulttemplatefile,'/etc/nginx/sites-available/default')
+    with open(defaultdoc) as inf:
+        txt = inf.read()
+        soup = bs4.BeautifulSoup(txt,features="html.parser")
+    bodytag = soup.body
+    bodytag.append("Virtualhost: {}".format(dnsname))
+    brtag = bs4.Tag(name = "br")
+    bodytag.append(brtag)
+    with open(defaultdoc,"w") as outf:
+        outf.write(str(soup))
 
     os.symlink(sitefile,'/etc/nginx/sites-enabled/{}'.format(dnsname))
 if __name__ == "__main__":
